fd174c618de109eb7d91b508ac6dd77b3d6fbc87
kauanAlmeida33 <105437759+kauanAlmeida33@users.noreply.github.com>
2023-10-24 07:43:41 -0300

    controle

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-function TocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function TocaSomPom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+document.querySelector('.tecla_pom').onclick = TocaSomPom
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+listaDeTeclas[0].onclick = TocaSomPom
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = TocaSomPom;
+    contador = contador + 1;
+    console.log(contador);
 }
