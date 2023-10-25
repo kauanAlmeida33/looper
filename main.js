b2325dcb4e38bbfb5d2c90e43d00d0239cb234a9
kauanAlmeida33 <105437759+kauanAlmeida33@users.noreply.github.com>
2023-10-25 11:26:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ let contador = 0;
 
 while(contador < listaDeTeclas.length){
     const efeito = listaDeTeclas[contador].classList[1];
-    const idAudio = #som_+efeito;
+    const idAudio = "#som_"+efeito;
     listaDeTeclas[contador].onclick = function(){
         TocaSom(idAudio);
     }
